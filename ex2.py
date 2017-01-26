92d0e24a955cddf74882d5e04840832aa52a0877
Iggi <iggirex@gmail.com>
2017-01-25 19:22:26 -0700

    init commit

---
+++ ex2.py
@@ -0,0 +1,13 @@
+print "I will now count my chickens"
+
+print "Roosters", 100 - 25 * 3 / 4
+
+print "Now I will count the eggs"
+
+print float(3 + 2 + 1 - 5 + 40 % 2)
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", float(3 + 2.5)
